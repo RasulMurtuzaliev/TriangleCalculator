447ff10a8351c0354b568c8bfe48d83eb9cc52aa
RasulMurtuzaliev <ras8705@mail.ru>
2024-01-31 16:33:31 +0500

    init

---
+++ src/main/java/Spar/TriangleCalculator/TriangleCalculatorApplication.java
@@ -0,0 +1,13 @@
+package Spar.TriangleCalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriangleCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TriangleCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Spar/TriangleCalculator/TriangleCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package Spar.TriangleCalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriangleCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
